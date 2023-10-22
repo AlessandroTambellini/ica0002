81ede9a9c2d76d0d0f62c605884c920d0c78219d
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2023-10-22 19:04:57 +0300

    fix host port substitution

--- retrive_IPs.sh
+++ retrive_IPs.sh
@@ -1,45 +1,25 @@
 #!/bin/bash
 
-#url="http://193.40.156.67/students/AlessandroTambellini.html"
-#
-#search_word="192.168.\{1,3\}.\{1,3\}"
-#
-#temp_file="$(mktemp)"
-#curl -s "$url" > "$temp_file"
-#
-## The -o option tells grep to only print the matched part of the line
-#if grep -o "$search_word" "$temp_file" > "test.txt"; then
-#  echo "The word '$search_word' was found in the HTML content."
-#else
-#  echo "The word '$search_word' was not found in the HTML content."
-#fi
-#
-## my name.csv
-## Clean up the temporary file
-#rm "$temp_file"
-
-#public_ip = "193.40.156.67"
-
 echo "Updating..." >&2
 
 host_info=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d, -f1,5)
+
+vm1_name=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d, -f1 | head -n 1)
+vm1_internal_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f2 | head -n 1)
+vm1_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | head -n 1)
+
+vm2_name=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 1 | sed -n '2p')
+vm2_internal_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 2 | sed -n '2p')
+vm2_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | sed -n '2p')
 
 # forget old hosts
 for port in $(echo "$host_info" | cut -d, -f2); do
     echo ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[$public_ip]:$port"
 done
 
-echo $host_info 
-
-# write new inventory
-# substitute "," with "ansible_port="
-hosts=$(cat << EOF
-$(echo "$host_info" | sed 's/,/ ansible_port=/')
-$(cat hosts | grep -v '^AlessandroTambellini-[0-9 ]*ansible')
-EOF
-)
-
-echo "$hosts" > hosts.backup
+vm_name="AlessandroTambellini-\w*"
+vm_port="ansible_port=\w*"
+sed -e "1 s/$vm_port/ansible_port=$vm1_port/1" -e "2 s/$vm_port/ansible_port=$vm2_port/1" -i hosts
 
 echo "Done." >&2
 git diff -- hosts
