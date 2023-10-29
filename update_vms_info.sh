76effa077342ba209036b4b029813deb3cd0d047
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2023-10-29 14:17:40 +0200

    update internal IP port automatically

--- update_vms_info.sh
+++ update_vms_info.sh
@@ -2,10 +2,10 @@
 
 echo "Updating hosts port..." >&2
 
-vm1_name=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d, -f1 | head -n 1)
-vm1_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | head -n 1)
+public_ip="193.40.156.67"
+host_info=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv")
 
-vm2_name=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 1 | sed -n '2p')
+vm1_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | head -n 1)
 vm2_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | sed -n '2p')
 
 # forget old hosts
@@ -13,8 +13,8 @@ for port in $(echo "$host_info" | cut -d, -f2); do
     echo ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[$public_ip]:$port"
 done
 
-vm_port="ansible_port=\w*"
-sed -e "1 s/$vm_port/ansible_port=$vm1_port/1" -e "2 s/$vm_port/ansible_port=$vm2_port/1" -i hosts
+ansible_port="ansible_port=\w*"
+sed -e "1 s/$ansible_port/ansible_port=$vm1_port/1" -e "2 s/$ansible_port/ansible_port=$vm2_port/1" -i hosts
 
 echo "Done." >&2
 git diff -- hosts
@@ -33,8 +33,12 @@ vm2_old_IP=$(grep -m 2 '\<192.168\w*' ./vms_IP.txt | tail -n 1)
 vm1_new_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f2 | head -n 1)
 vm2_new_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 2 | sed -n '2p')
 
-# substitute new IPs with old ones
-sed -e "s/$vm1_old_IP/$vm1_new_IP/" -e "s/$vm2_old_IP/$vm2_new_IP/" -i ./group_vars/all.yaml
+# substitute old IPs and ports with new ones
+sed -e "s/$vm1_old_IP/$vm1_new_IP/" \
+    -e "s/$vm2_old_IP/$vm2_new_IP/" \
+    -e "s/vm1_port: \w*/vm1_port: $vm1_port/1" \
+    -e "s/vm2_port: \w*/vm1_port: $vm2_port/1" \
+    -i ./group_vars/all.yaml
 
 # save the new IPs in vms_IP.txt
 sed -e "s/$vm1_old_IP/$vm1_new_IP/" -e "s/$vm2_old_IP/$vm2_new_IP/" -i ./vms_IP.txt
