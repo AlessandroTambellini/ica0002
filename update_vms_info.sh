1551fc55c457c21696328adfecf1efd99e1c86dd
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2023-11-07 14:20:59 +0200

    setup grafana dashboard

--- update_vms_info.sh
+++ update_vms_info.sh
@@ -5,8 +5,8 @@ echo "Updating hosts port..." >&2
 public_ip="193.40.156.67"
 host_info=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv")
 
-vm1_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | head -n 1)
-vm2_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | sed -n '2p')
+vm1_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | head -n 1)
+vm2_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | sed -n '2p')
 
 # forget old hosts
 for port in $(echo "$host_info" | cut -d, -f2); do
@@ -14,7 +14,7 @@ for port in $(echo "$host_info" | cut -d, -f2); do
 done
 
 ansible_port="ansible_port=\w*"
-sed -e "1 s/$ansible_port/ansible_port=$vm1_port/1" -e "2 s/$ansible_port/ansible_port=$vm2_port/1" -i hosts
+sed -e "1 s/$ansible_port/ansible_port=$vm1_public_SSH_port/1" -e "2 s/$ansible_port/ansible_port=$vm2_public_SSH_port/1" -i hosts
 
 echo "Done." >&2
 git diff -- hosts
@@ -23,25 +23,24 @@ git diff -- hosts
 #
 #
 
-echo "Updaring vms internal IP..." >&2
+echo "Updating variables..." >&2
 
 # read old IPs from vms_IP.txt
-vm1_old_IP=$(grep -m 1 "\<192.168\w*" ./vms_IP.txt)
-vm2_old_IP=$(grep -m 2 '\<192.168\w*' ./vms_IP.txt | tail -n 1)
+vm1_old_internal_IP=$(grep -m 1 "\<192.168\w*" ./vms_IP.txt)
+vm2_old_internal_IP=$(grep -m 2 '\<192.168\w*' ./vms_IP.txt | tail -n 1)
 
 # read new IPs from personal .csv file
-vm1_new_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f2 | head -n 1)
-vm2_new_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 2 | sed -n '2p')
+vm1_new_internal_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f2 | head -n 1)
+vm2_new_internal_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 2 | sed -n '2p')
 
+# sesto campo
 # substitute old IPs and ports with new ones
-sed -e "s/$vm1_old_IP/$vm1_new_IP/" \
-    -e "s/$vm2_old_IP/$vm2_new_IP/" \
-    -e "s/vm1_port: \w*/vm1_port: $vm1_port/1" \
-    -e "s/vm2_port: \w*/vm2_port: $vm2_port/1" \
-    -i ./group_vars/all.yaml
+#sed -e "0,/public_SSH_port: \w*/{s/public_SSH_port: \w*/public_SSH_port: $vm1_public_SSH_port}" \
+#    -e "s/public_SSH_port: \w*/public_SSH_port: $vm2_public_SSH_port/2" \
+#    -i ./group_vars/all.yaml
 
 # save the new IPs in vms_IP.txt
-sed -e "s/$vm1_old_IP/$vm1_new_IP/" -e "s/$vm2_old_IP/$vm2_new_IP/" -i ./vms_IP.txt
+sed -e "s/$vm1_old_internal_IP/$vm1_new_internal_IP/" -e "s/$vm2_old_internal_IP/$vm2_new_internal_IP/" -i ./vms_IP.txt
 
 echo "Done." >&2
 git diff -- ./group_vars/all.yaml
