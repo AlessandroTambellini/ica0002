bc9f9b555e73f75a30f0a1b3784306af02e42144
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2023-11-12 14:21:12 +0200

    add minor updates: 	- syntax conventions 	- redundancy 	- usage of variables

--- update_vms_info.sh
+++ update_vms_info.sh
@@ -34,6 +34,8 @@ echo "Updating variables..." >&2
 
 vm1_public_URL=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 6 | head -n 1)
 vm2_public_URL=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 6 | sed -n '2p')
+# TODO: I don't know if backup server IP is in this file
+backup_server_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 7 | sed -n '2p')
 
 echo $vm1_public_URL
 
@@ -45,6 +47,10 @@ URL1=$vm1_public_URL URL2=$vm2_public_URL yq -i '
 SSH1=$vm1_public_SSH_port SSH2=$vm2_public_SSH_port yq -i '
     .vms.vm1.public_SSH_port = strenv(SSH1) |
     .vms.vm2.public_SSH_port = strenv(SSH2)
+' ./group_vars/all.yaml
+
+BACKUP_IP=$backup_server_IP yq -i '
+    .backup_server_IP = strenv(BACKUP_IP)
 ' ./group_vars/all.yaml
 
 # git diff -- ./group_vars/all.yaml
