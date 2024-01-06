c19b03c8bdccaa7eca763e0a1577e75cde601482
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2024-01-06 18:02:19 +0100

    restructure the infrastructure on 3 vms

--- update_vms_info.sh
+++ update_vms_info.sh
@@ -18,10 +18,14 @@ done
 #
 
 ansible_port="ansible_port=\w*"
-vm1_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | head -n 1)
-vm2_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 5 | sed -n '2p')
+vm1_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | awk -F ',' 'NR==1 {print $5}')
+vm2_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | awk -F ',' 'NR==2 {print $5}')
+vm3_public_SSH_port=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | awk -F ',' 'NR==3 {print $5}')
 
-sed -e "1 s/$ansible_port/ansible_port=$vm1_public_SSH_port/1" -e "2 s/$ansible_port/ansible_port=$vm2_public_SSH_port/1" -i hosts
+sed -e "1 s/$ansible_port/ansible_port=$vm1_public_SSH_port/1" \
+    -e "2 s/$ansible_port/ansible_port=$vm2_public_SSH_port/1" \
+    -e "3 s/$ansible_port/ansible_port=$vm3_public_SSH_port/1" \
+    -i hosts
 
 echo "Done." >&2
 #git diff -- hosts
@@ -36,15 +40,6 @@ vm1_public_URL=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" |
 vm2_public_URL=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 6 | sed -n '2p')
 vm1_internal_IP=$(curl "http://193.40.156.67/students/AlessandroTambellini.csv" | cut -d "," -f 2 | head -n 1)
 
-third_octet=$(echo "$vm1_internal_IP" | awk -F'.' '{print $3}')
-if [[ $third_octet -eq 42 ]]; then
-    virtual_ipaddress="${vm1_internal_IP/42/100}"
-elif [[ $third_octet -eq 43 ]]; then
-    virtual_ipaddress="${vm1_internal_IP/43/101}"
-else
-    virtual_ipaddress=""
-fi
-
 URL1=$vm1_public_URL URL2=$vm2_public_URL yq -i '
     .vm1_public_URL = strenv(URL1) |
     .vm2_public_URL = strenv(URL2)
@@ -54,11 +49,10 @@ iIP1=$vm1_internal_IP yq -i '
     .dns_forwarders[0] = strenv(iIP1)
 ' ./group_vars/all.yaml
 
-vip=$virtual_ipaddress yq -i '
-    .virtual_ipaddress = strenv(vip)
-' ./group_vars/all.yaml
-
 #git diff -- ./group_vars/all.yaml
 echo "Done." >&2
 
-echo -e "ports: \nvm1: $vm1_public_SSH_port\nvm2: $vm2_public_SSH_port"
+echo -e "ports: \n \
+        vm1: $vm1_public_SSH_port\n \
+        vm2: $vm2_public_SSH_port\n \
+        vm3: $vm3_public_SSH_port"
